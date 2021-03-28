7c36d08636741b62a8caa73790684092e3715cf1
Lenovo <2192877932@qq,com>
2021-03-28 21:19:00 +0800

    HW week4 homework

---
+++ src/main/java/com/yancheng/week4/demo/JDBCDemoServlet.java
@@ -0,0 +1,83 @@
+package com.yancheng.week4.demo;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+//use @webservlet - no web.xml code
+@WebServlet(
+        urlPatterns = {"/jdbc"},
+        initParams = {
+                @WebInitParam(name="driver",value = "com.microsoft.sqlserver.jbc.SQLserverDriver"),
+                @WebInitParam(name="url",value = "jdbc:sqlserver://localhost;databaseName=MDSK;"),
+                @WebInitParam(name="username",value = "sa"),
+                @WebInitParam(name="password",value = "YanLaoBan" )
+        },loadOnStartup = 1
+
+)//end of webservlet
+public class JDBCDemoServlet extends HttpServlet {
+    Connection con=null;//class variable
+    @Override
+    public void init() throws ServletException{
+        //only one connection
+        //String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        //String url="jdbc:sqlserver://localhost:databaseName=MDSK;";
+        //String username="sa";
+        //String password="YanLaoBan";
+        //code like this is bad way ---because change in not easy
+        //for example change password of db- change in java code
+
+        //get servletconfig --> getInitParameters
+        ServletConfig config=getServletConfig();
+        String driver=config.getInitParameter("driver");//<param-name>driver</param-name>
+        String url=config.getInitParameter("url");
+        String username=config.getInitParameter("username");
+        String password=config.getInitParameter("password");
+
+        try{
+            Class.forName(driver);
+            con= DriverManager.getConnection(url,username,password);
+            System.out.println("init()-->"+con);// ok(java code)-ok(use config-in web.xml) -- ok -use( @webservlet )
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+     // connection within do get--many times - bad may
+        System.out.println("i am in doGet()");//ok
+        //we need to use con within doget
+        String sql="select * from usertable";
+        try{
+            ResultSet rs=con.createStatement().executeQuery(sql);
+            while (rs.next()){
+                //get from rs - print - write into response
+            }
+        }catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void destroy(){
+        super.destroy();
+        try{
+            con.close();//when tomcat stop
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+    }
+
+}
